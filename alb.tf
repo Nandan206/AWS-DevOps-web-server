7180947998b30f7cd20fe6b0021af359186ace45
Nandan <167064786+Nandan206@users.noreply.github.com>
2025-03-21 21:47:39 +0530

    Terraform code for Load Balancing

---
+++ alb.tf
@@ -0,0 +1,25 @@
+resource "aws_lb" "my_alb" {
+  name               = "my-app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets           = ["subnet-12345", "subnet-67890"] # Replace with your subnet IDs
+}
+
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-xxxxx"  # Replace with your VPC ID
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_tg.arn
+  }
+}
