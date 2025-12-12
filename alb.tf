9147e9da7b751d3205342911743bdb759a51f0f1
Nandan <167064786+Nandan206@users.noreply.github.com>
2025-12-12 08:30:36 +0530

    Reformat alb.tf for consistency

--- alb.tf
+++ alb.tf
@@ -3,14 +3,14 @@ resource "aws_lb" "my_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets           = ["subnet-12345", "subnet-67890"] # Replace with your subnet IDs
+  subnets           = ["subnet-12345", "subnet-67890"]
 }
 
 resource "aws_lb_target_group" "my_tg" {
   name     = "my-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-xxxxx"  # Replace with your VPC ID
+  vpc_id   = "vpc-xxxxx"  
 }
 
 resource "aws_lb_listener" "http" {
@@ -23,3 +23,4 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.my_tg.arn
   }
 }
+
